b9a09f167f656f3d1647e502d5a2dd8387cbadd4
Connor Peet <connor@peet.io>
2017-09-14 18:43:33 -0700

    Add ParallelChunks function

--- msync/parallel_test.go
+++ msync/parallel_test.go
@@ -9,12 +9,20 @@ import (
 )
 
 func parallelTester(max, workers int, ch chan<- int) {
-	Parallel(0, max, workers, func(x int) {
+	Parallel(max, workers, func(x int) {
 		ch <- x
 	})
 }
 func parallelRangesTester(max, workers int, ch chan<- int) {
-	ParallelRanges(0, max, workers, func(start, end int) {
+	ParallelRanges(max, workers, func(start, end int) {
+		for i := start; i < end; i++ {
+			ch <- i
+		}
+	})
+}
+
+func parallelChunkTester(max, workers int, ch chan<- int) {
+	ParallelChunks(max, 3, workers, func(start, end int) {
 		for i := start; i < end; i++ {
 			ch <- i
 		}
@@ -50,8 +58,45 @@ func benchmarkRangeGenerator(b *testing.B, fn func(max, workers int, ch chan<- i
 	fn(b.N, runtime.GOMAXPROCS(0), ch)
 }
 
-func TestParallel(t *testing.T)      { testRangeGenerator(t, parallelTester) }
-func TestParallelRange(t *testing.T) { testRangeGenerator(t, parallelRangesTester) }
+func TestParallel(t *testing.T)       { testRangeGenerator(t, parallelTester) }
+func TestParallelRange(t *testing.T)  { testRangeGenerator(t, parallelRangesTester) }
+func TestParallelChunks(t *testing.T) { testRangeGenerator(t, parallelChunkTester) }
 
-func BenchmarkParallel(b *testing.B)      { benchmarkRangeGenerator(b, parallelTester) }
-func BenchmarkParallelRange(b *testing.B) { benchmarkRangeGenerator(b, parallelRangesTester) }
+func TestParallelChunksSizes(t *testing.T) {
+	max := 100
+	var expected []int
+	for i := 0; i < max; i++ {
+		expected = append(expected, i)
+	}
+
+	for chunkSize := 1; chunkSize < 100; chunkSize++ {
+		for workers := 1; workers < 5; workers++ {
+			ch := make(chan int, max)
+			ParallelChunks(max, chunkSize, workers, func(start, end int) {
+				if end-start > chunkSize {
+					t.Fatal("expected chunk size of", chunkSize, "got", end-start)
+				}
+				if end-start < chunkSize && end != max {
+					t.Fatal("expected to only get smaller chunk at end of generator")
+				}
+
+				for i := start; i < end; i++ {
+					ch <- i
+				}
+			})
+			close(ch)
+
+			var actual []int
+			for n := range ch {
+				actual = append(actual, n)
+			}
+
+			sort.Sort(sort.IntSlice(actual))
+			assert.Equal(t, expected, actual)
+		}
+	}
+}
+
+func BenchmarkParallel(b *testing.B)       { benchmarkRangeGenerator(b, parallelTester) }
+func BenchmarkParallelRange(b *testing.B)  { benchmarkRangeGenerator(b, parallelRangesTester) }
+func BenchmarkParallelChunks(b *testing.B) { benchmarkRangeGenerator(b, parallelChunkTester) }
