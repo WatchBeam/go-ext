5ae263f837521e768bf3400ac15f2a4887f4f8bf
Connor Peet <connor@peet.io>
2017-09-14 18:43:33 -0700

    Add ParallelChunks function

--- msync/parallel.go
+++ msync/parallel.go
@@ -7,49 +7,29 @@ import (
 
 // Parallel spawns `workers` goroutines and invokes the function in each
 // routine from `start` to `end`. It blocks until all functions return.
-func Parallel(start, end, workers int, fn func(i int)) {
-	if end-start == 0 {
-		return
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(workers)
-
-	current := uint64(start)
-	for i := 0; i < workers; i++ {
-		go func() {
-			for {
-				x := int(atomic.AddUint64(&current, 1))
-				if x > end {
-					wg.Done()
-					return
-				}
-
-				fn(x - 1)
-			}
-		}()
-	}
-
-	wg.Wait()
+func Parallel(count, workers int, fn func(i int)) {
+	ParallelChunks(count, 1, workers, func(start, _ int) { fn(start) })
 }
 
 // ParallelRanges divides up the the domain into segments and dispatches
 // them to worker functions.
-func ParallelRanges(start, end, workers int, fn func(start, end int)) {
-	if end-start == 0 {
+func ParallelRanges(count, workers int, fn func(start, end int)) {
+	if count == 0 {
 		return
 	}
-	if end-start < workers {
-		fn(start, end)
+	if count < workers {
+		fn(0, count)
 		return
 	}
 
+	segment := count / workers
+
 	var wg sync.WaitGroup
 	for i := 0; i < workers; i++ {
-		a := i * start / end
-		b := (i + 1) * start / end
+		a := i * segment
+		b := (i + 1) * segment
 		if i == workers-1 {
-			b = end
+			b = count
 		}
 		if b-a == 0 {
 			continue
@@ -63,4 +43,47 @@ func ParallelRanges(start, end, workers int, fn func(start, end int)) {
 	}
 
 	wg.Wait()
+}
+
+// ParallelChunks divides the domain into chunks of "chunkSize" and feeds it
+// to the function, with bounded concurrency of `worker` functions.
+func ParallelChunks(count, chunkSize, workers int, fn func(start, end int)) {
+	if count == 0 {
+		return
+	}
+	if chunkSize == 0 {
+		panic("Cannot pass a chunkSize of 0 to ParallelChunks")
+	}
+	if count < chunkSize {
+		fn(0, count)
+		return
+	}
+
+	var (
+		wg      sync.WaitGroup
+		offset  int64
+		chunk64 = int64(chunkSize)
+	)
+
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		go func() {
+			for {
+				upper := int(atomic.AddInt64(&offset, chunk64))
+				lower := upper - chunkSize
+				if lower >= count {
+					wg.Done()
+					return
+				}
+				if upper > count {
+					upper = count
+				}
+
+				fn(lower, upper)
+			}
+		}()
+	}
+
+	wg.Wait()
+
 }
